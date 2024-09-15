679eea9a3ac5e0d909775a26a8f2b22d389646b9
dubwub <dingdarwin@gmail.com>
2024-09-14 20:25:52 -0400

    basic potato structure coming together

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  // potato logic
+  let numPotatoLines = Math.floor(Math.random() * 5 + 3)
+  let potatoCount = 0
+
+  let potatoes = [];
+  for (let i = 0; i < numPotatoLines; i++) {
+    let add = [];
+    for (let j = 0; j < 10; j++) {
+      if (Math.random() < 0.33) { // 33% chance of being a potato gap
+        add.push(<div style={{display: 'inline-block', width: 40, height: 40}} />)
+      } else {
+        potatoCount++;
+        add.push(
+          <img src={'/potato' + (Math.floor(Math.random() * 3) + 1) + '.gif'} style={{display: 'inline-block', margin: 0, padding: 0}} />
+        )
+      }
+    }
+    add.push(<br/>)
+    potatoes.push(add)
+  }
+
+  // guesser logic
+  let inputVal = "";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'/potato_think.gif'} alt="logo" /><br/>
+        {
+          potatoes
+        }
+        <input onChange={(e) => inputVal = e.target.value}/>
+        {inputVal}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
